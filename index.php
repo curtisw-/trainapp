0715aa48921db195cf87166173c893d38ecd8828
Curtis Warren <curtisw000@gmail.com>
2014-03-18 14:24:56 -0400

    Added some styling

--- index.php
+++ index.php
@@ -3,33 +3,38 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
+		<link rel="stylsheet" href="css/reset.css">
 		<link rel="stylesheet" href="css/main.css">
 	</head>
 
 	<body>
-		<div id="traindata">
-			<table border=0>
-				<tr>
-					<th>Train Line</th>
-					<th>Route</th>
-					<th>Run Number</th>
-					<th>Operator ID</th>
-				</tr>
-				<tr>
-					<td>line1</t>
-					<td>apples</td>
-					<td>403d</td>
-					<td>Henry</td>
-				</tr>
-			</table>
+		<div id="header">
+			<h3>Train App v1.0</h3>
 		</div>
+		<div id="contents">
+			<div id="traindata">
+				<table border=0>
+					<tr>
+						<th>Train Line</th>
+						<th>Route</th>
+						<th>Run Number</th>
+						<th>Operator ID</th>
+					</tr>
+					<tr>
+						<td>line1</t>
+						<td>apples</td>
+						<td>403d</td>
+						<td>Henry</td>
+					</tr>
+				</table>
+			</div>
 		
-		<div id="uploadform">
-			<form method="POST" action="uploadcsv.php">
-				<label>Upload data: <input type="file" name="csv"></label>
-			</form>
+			<div id="uploadform">
+				<form method="POST" action="uploadcsv.php">
+					<label>Upload data: <input type="file" name="csv"></label>
+				</form>
+			</div>
 		</div>
-		
 	</body>
 
 </html>
