1318666a3450028247c64f772756054528578069
Curtis Warren <curtisw000@gmail.com>
2014-03-18 17:12:50 -0400

    First test passed

--- inc/CSV.php
+++ inc/CSV.php
@@ -1,10 +1,18 @@
 <?php
 
 
+class UnreadableFileException extends Exception { }
+
+
 class CSV {
 
+	private $handle;
+
 	public function __construct($filename) {
-		
+		$handle = @fopen($filename, "r");
+		if(!$handle) {
+			throw new UnreadableFileException();
+		}
 	}
 
 }
--- tests/CSVTest.php
+++ tests/CSVTest.php
@@ -6,11 +6,11 @@ require_once 'inc/CSV.php';
 class CSVTest extends PHPUnit_Framework_TestCase {
 
 	/**
-	 * @expectedException NoSuchFileException
+	 * @expectedException UnreadableFileException
 	 */
 	public function testFailureOnNonexistantFile() {
 		$file = tempnam("/tmp", "csv");
-		
+		unlink($file);
 		$csv = new CSV($file);
 	}
 
