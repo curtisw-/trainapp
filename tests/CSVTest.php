760c97826cab46d3ea72a08335c4128b0c4fea5c
curtisw- <curtisw000@gmail.com>
2014-03-18 23:48:28 -0500

    added comments

--- tests/CSVTest.php
+++ tests/CSVTest.php
@@ -9,18 +9,22 @@ class CSVTest extends PHPUnit_Framework_TestCase {
 	 * @expectedException UnreadableFileException
 	 */
 	public function testFailureOnNonexistantFile() {
+		// Make sure it throws an exception when given a nonexistant file
 		$file = tempnam("/tmp", "csv");
 		unlink($file);
 		$csv = new CSV($file);
 	}
 	
-	public function testSuccessOnExtantFile() {
+	public function testSuccessOnExistingFile() {
+		// Make sure no exceptions are thrown when given an existing file
 		$file = tempnam("/tmp", "csv");
 		file_put_contents($file, "");
 		$csv = new CSV($file);
 	}
 
 	public function testFileRead() {
+		// Make sure the contents of the csv file are parsed correctly, especially
+		// with regards to spaces and escaped commas.
 		$file = tempnam("/tmp", "csv");
 		
 		$contents = <<<END
